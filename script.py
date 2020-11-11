79d036b58fce4274a3f7de51f1f22a551a9ae54c
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2020-11-11 11:41:31 -0800

    Made URL more readable

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import requests
 import json
 from datetime import date
-
-x = requests.get('https://www.tesla.com/inventory/api/v1/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22m3%22%2C%22condition%22%3A%22used%22%2C%22options%22%3A%7B%22TRIM%22%3A%5B%22MRRWD%22%2C%22LRRWD%22%2C%22LRAWD%22%5D%2C%22AUTOPILOT%22%3A%5B%22AUTOPILOT_FULL_SELF_DRIVING%22%5D%7D%2C%22arrangeby%22%3A%22Price%22%2C%22order%22%3A%22asc%22%2C%22market%22%3A%22US%22%2C%22language%22%3A%22en%22%2C%22super_region%22%3A%22north%20america%22%2C%22lng%22%3A-122.1257%2C%22lat%22%3A47.6722%2C%22zip%22%3A%2298052%22%2C%22range%22%3A0%7D%2C%22offset%22%3A0%2C%22count%22%3A50%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%7D')
+import urllib
+query = '{"query":{"model":"m3","condition":"used","options":{"TRIM":["MRRWD","LRRWD","LRAWD"],"AUTOPILOT":["AUTOPILOT_FULL_SELF_DRIVING"]},"arrangeby":"Price","order":"asc","market":"US","language":"en","super_region":"north america","lng":-122.1257,"lat":47.6722,"zip":"98052","range":0},"offset":0,"count":50,"outsideOffset":0,"outsideSearch":false}'
+x = requests.get('https://www.tesla.com/inventory/api/v1/inventory-results?query=' + urllib.parse.quote(query))
 y = json.loads(x.content)
 today = date.today()
 toExport = ('"Found ' + y['total_matches_found'] + ' results."\n')
